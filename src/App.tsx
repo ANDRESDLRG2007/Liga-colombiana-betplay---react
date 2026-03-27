6bfb8401ddf63609a90e177366a8200fa3b1fa01
Andres DLRG <cristian.rueg@uniagustiniana.edu.co>
2026-03-27 19:16:53 +0000

    Clase 27/03 - se agrego la estructura routing usando React Router. - Se crearon los componentes: Home, Favoritos, Equipo, Informativa, Usuario, y Original.

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router';
+import './App.css';
+import Home from "./Home";
+import Favoritos from "./Favoritos";
+import Equipo from "./Equipo";
+import Informativa from "./Informativa";
+import Usuario from "./Usuario";
+import Original from "./Original";
+
+function App() {
+  return (
+    <Router>
+      <nav className='c-menu'>
+        <Link to="/"></Link>
+        <Link to="/favoritos">Favoritos</Link>
+        <Link to="/original">Original</Link>
+        <Link to="/informativa">Informativa</Link>
+        <Link to="/usuario">Usuario</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+        <Route path="/equipo" element={<Equipo />} />
+        <Route path="/informativa" element={<Informativa />} />
+        <Route path="/usuario" element={<Usuario />} />
+        <Route path="/original" element={<Original />} />
+        <Route path="/home" element={<Home />} />
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
